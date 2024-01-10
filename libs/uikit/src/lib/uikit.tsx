813ae78f197f0028a67e0ee0451b1299e2c9502f
afyifah <mailwork.afifah@gmail.com>
2024-01-10 20:47:20 +0700

    initial commit

---
+++ libs/uikit/src/lib/uikit.tsx
@@ -0,0 +1,14 @@
+import './uikit.css';
+
+/* eslint-disable-next-line */
+export interface UikitProps {}
+
+export function Uikit(props: UikitProps) {
+  return (
+    <div>
+      <h1 style={{backgroundColor: 'red' }}>Welcome to Uikit!</h1>
+    </div>
+  );
+}
+
+export default Uikit;
