6e05b59a1d631f641a0ed4d62812dc655e29cc76
afyifah <mailwork.afifah@gmail.com>
2024-01-12 04:18:13 +0700

    chore: add nanoid

--- apps/user-ui/app/dashboard/action.ts
+++ apps/user-ui/app/dashboard/action.ts
@@ -1,14 +1,20 @@
 "use server"
 
 import {prisma} from "@ukmersid/prisma"
+import { revalidatePath } from "next/cache";
+import { customAlphabet } from 'nanoid'
 
 export async function createTicket(formData : FormData) {
     const title = formData.get('title');
     const description = formData.get('description');
     
+    const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz_', 13);
+    const id = `dv__${nanoid()}`; //=>"4f90d13a42"
+    console.log(id);
+
     const createNewTicket = await prisma.ticket.create({
         data:{
-            id:'2',
+            id,
             title: title as string ,
             description: description as string,
             status: 'OPEN',
@@ -16,4 +22,5 @@ export async function createTicket(formData : FormData) {
         },
     });
     console.log(createNewTicket);
+    revalidatePath('/dashboard ');
 }
